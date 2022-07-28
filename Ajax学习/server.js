49e4ebd573686302c7685a67a5c6ff8883591794
U2yyy <a1535887812@gmail.com>
2022-07-28 18:46:53 +0800

    JSON文件的响应与请求

--- Ajax学习/server.js
+++ Ajax学习/server.js
@@ -14,6 +14,16 @@ app.post('/server',(require,response)=>{
     response.send("Hello Ajax");
 })
 
+app.all('/json-server',(require,response)=>{
+    //设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*');
+    const date = {
+        name:'Hello World'
+    };
+    let str = JSON.stringify(date);
+    response.send(str);
+})
+
 app.listen(8000,()=>{
     console.log("8000端口监听中...");
 })
