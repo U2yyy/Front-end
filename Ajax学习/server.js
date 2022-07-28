01571c2796cc4c98cfbeb2d9d926320b7ec61c07
U2yyy <a1535887812@gmail.com>
2022-07-28 17:11:55 +0800

    POST操作实现

--- Ajax学习/server.js
+++ Ajax学习/server.js
@@ -8,6 +8,12 @@ app.get('/server',(require,response)=>{
     response.send("Hello Ajax");
 })
 
+app.post('/server',(require,response)=>{
+    //设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*');
+    response.send("Hello Ajax");
+})
+
 app.listen(8000,()=>{
     console.log("8000端口监听中...");
 })
