792b4df72687494350a5dc11a50b887c61280257
U2yyy <a1535887812@gmail.com>
2022-08-07 22:28:53 +0800

    基于Vue2和脚手架的第一次组件化开发试验

---
+++ Vue_advance/vue-first/src/components/MySchool.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="school">
+    <h1>MySchool</h1>
+    <br>
+    <h2>name:{{name}}</h2>
+    <h2>address:{{address}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MySchool",
+  data(){
+    return{
+      name:'Huazhong-University',
+      address:'Wuhan'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #school{
+    border: dotted burlywood;
+  }
+</style>
---
+++ Vue_advance/vue-first/src/components/MyStudent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="student">
+    <h1>MyStudent</h1>
+    <br>
+    <h2>name:{{name}}</h2>
+    <h2>age:{{age}}</h2>
+    <button @click="change">Click me to change student</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyStudent",
+  data(){
+    return {
+      name:'U2y',
+      age:20
+    }
+  },
+  methods:{
+    change(){
+      this.name = 'YME';
+      this.age = 19
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #student{
+    background: #2aabd2;
+    border: solid aquamarine;
+  }
+  h2{
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: larger;
+    font-style: inherit;
+  }
+  button{
+    font-style: initial;
+    font-size: 30px;
+    height: 50px;
+  }
+</style>
