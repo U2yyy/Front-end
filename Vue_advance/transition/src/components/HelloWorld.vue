f66a43e3849389e082f981afa3852ef5bb514b59
U2yyy <a1535887812@gmail.com>
2022-08-23 18:36:40 +0800

    用Vue动画过渡的初步实现

---
+++ Vue_advance/transition/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <button @click="isShow=!isShow">Click to change the element</button>
+    <transition name="hello" appear>
+      <h1 v-show="isShow">HelloWorld!</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      isShow:true,
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello-enter-active{
+    animation:myTransition 1s linear;
+  }
+  .hello-leave-active{
+    animation:myTransition 1s linear reverse;
+  }
+  @keyframes myTransition {
+    from{
+      transform: translateX(-100%) ;
+    }
+    to{
+      transform: translateX(0px);
+    }
+  }
+</style>
