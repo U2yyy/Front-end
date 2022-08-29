2f8ba955670f97633707843a6d2ce5a579056cdb
U2yyy <a1535887812@gmail.com>
2022-08-29 15:39:07 +0800

    插槽，其中作用域插槽似乎不可用，Vue似乎已经移除

---
+++ Vue_advance/slot/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+<!--    <test>-->
+<!--      <template slot="mySlot">-->
+<!--        <h1>{{names}}</h1>-->
+<!--      </template>-->
+<!--    </test>-->
+    <test>
+      <template slot-scope="yme">
+        <h1>{{yme.name}}</h1>
+      </template>
+    </test>
+  </div>
+</template>
+
+<script>
+import test from "@/components/test";
+export default {
+  name: 'App',
+  components: {
+    test
+  },
+  data(){
+    return{
+      names:'U2y'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue_advance/slot/src/components/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+<!--    <slot name="mySlot">默认内容</slot>-->
+    <slot :name="name">默认内容</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data(){
+    return{
+      name:'YME'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
